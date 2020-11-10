8955c0da0cdf009edbae1df7e2c4cc7cdbee1123
Lari Liuhamo <Diapolo10@users.noreply.github.com>
2020-11-10 18:59:26 +0200

    Added a description to PyPI

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 #! /usr/bin/env python3
 
 from setuptools import setup, find_packages # type: ignore
+from pathlib import Path
+
+with open(Path(__file__).parent / 'README.md', encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='iplib3',
-    version='0.1.0',
+    version='0.1.1',
     description="A modern, object-oriented approach to IP addresses.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author="Lari Liuhamo",
     author_email='lari.liuhamo+pypi@gmail.com',
-    url='https://github.com/Diapolo10/iplib',
+    url='https://github.com/Diapolo10/iplib3',
     packages=find_packages(include=['iplib3', 'iplib3.*']),
     install_requires=[],
     setup_requires=['pytest-runner', 'flake8'],
