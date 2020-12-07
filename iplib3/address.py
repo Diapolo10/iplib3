5c993d5833ae971c00a17291c6c99336fa47d349
Lari Liuhamo <Diapolo10@users.noreply.github.com>
2020-12-07 04:33:49 +0200

    Added badges and additional constants

--- iplib3/address.py
+++ iplib3/address.py
@@ -9,19 +9,23 @@ PORT_NUMBER_MAX_VALUE = 65535 # 2 ** 16 - 1 == 0xFFFF
 
 # IPv4 constants
 IPV4_MAX_SEGMENT_COUNT = 4
+IPV4_MIN_SEGMENT_COUNT = 4 # IPv4 shortening is not valid
 IPV4_MAX_SEGMENT_VALUE = 0xFF # (255)
+IPV4_MIN_SEGMENT_VALUE = 0x0 # (0)
+IPV4_MAX_SUBNET_VALUE = 8 * 4 - 1 # == 31, 8 bits in 4 segments, -1 to have room for at least one device
 IPV4_MIN_SUBNET_VALUE = 0 # Often 1 is a better choice as in some systems 0 has a
                           # special meaning, but technically 0 is valid
-IPV4_MAX_SUBNET_VALUE = 8 * 4 - 1 # == 31, 8 bits in 4 segments, -1 to have room for at least one device
 # 0xFF_FF_FF_FF (8)
 IPV4_MAX_VALUE = 4294967295 # 0xFF*0x100**3 + 0xFF*0x100**2 + 0xFF*0x100**1 + 0xFF*0x100**0
 IPV4_MIN_VALUE = 0 # 0x0*0x100**0
 
 # IPv6 constants
 IPV6_MAX_SEGMENT_COUNT = 8
+IPV6_MIN_SEGMENT_COUNT = 0 # Technically legal as long as there are at least two colons (::)
 IPV6_MAX_SEGMENT_VALUE = 0xFFFF # (65535)
-IPV6_MIN_SUBNET_VALUE = 0 # Unlike in IPV4, this should *always* be valid
+IPV6_MIN_SEGMENT_VALUE = 0x0 # (0)
 IPV6_MAX_SUBNET_VALUE = 16 * 8 - 1 # == 127, 16 bits in 8 segments, -1 to have room for at least one device
+IPV6_MIN_SUBNET_VALUE = 0 # Unlike in IPV4, this should *always* be valid
 # 0xFFFF_FFFF_FFFF_FFFF_FFFF_FFFF_FFFF_FFFF (32)
 IPV6_MAX_VALUE = 340282366920938463463374607431768211455 # 0xFFFF*0x10_000**7 + ... + 0xFFFF*0x10_000**0
 IPV6_MIN_VALUE = 0 # 0x0*0x10_000**0
