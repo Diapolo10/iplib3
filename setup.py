784201ed8ebc9b6d9ff8a26e4649c85b50a66aaa
Lari Liuhamo <Diapolo10@users.noreply.github.com>
2021-05-01 21:51:41 +0300

    Fingers crossed it works this time

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(Path(__file__).parent / 'README.md', encoding='utf-8') as f:
 
 setup(
     name='iplib3',
-    version='0.1.5-4',
+    version='0.1.5-5',
     description="A modern, object-oriented approach to IP addresses.",
     license="MIT License",
     long_description=long_description,
